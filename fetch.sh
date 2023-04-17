c5696fb51add44e46b5169b0613a48ecb201ee21
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-17 15:50:19 +0800

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -5,8 +5,6 @@ get_latest_release() {
 }
 mkdir openwrt-packages
 
-git clone -b js --depth 1 https://github.com/waynesg/OpenWrt-Software
-
 #git clone --depth=1 --branch v5 https://github.com/sbwml/luci-app-mosdns.git sbwml-packages
 #cp -r sbwml-packages/luci-app-mosdns openwrt-packages/luci-app-mosdns
 #cp -r sbwml-packages/v2dat openwrt-packages/v2dat
@@ -28,11 +26,11 @@ git clone -b js --depth 1 https://github.com/waynesg/OpenWrt-Software
 #cp -r OpenClash/luci-app-openclash openwrt-packages/luci-app-openclash
 #rm -rf OpenClash
 
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git openwrt-packages/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config openwrt-packages/luci-app-argon-config
+#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git openwrt-packages/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config openwrt-packages/luci-app-argon-config
 
-#git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git openwrt-packages/luci-app-temp-status
-#git clone --depth=1 https://github.com/waynesg/luci-app-tn-netports.git openwrt-packages/luci-app-tn-netports
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git openwrt-packages/luci-app-temp-status
+git clone --depth=1 https://github.com/waynesg/luci-app-tn-netports.git openwrt-packages/luci-app-tn-netports
 #git clone --depth=1 https://github.com/waynesg/luci-app-internet-detector.git waynesg
 #cp -r waynesg/luci-app-internet-detector openwrt-packages/luci-app-internet-detector
 #cp -r waynesg/internet-detector openwrt-packages/internet-detector
